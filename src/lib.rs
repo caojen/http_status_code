8901cfbcfd67ad2dd0da712a6ff447b397feda51
caojen <caojen@mail2.sysu.edu.cn>
2023-10-12 16:23:11 +0800

    feat: lib rename to rhttp_status_code

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 //! HTTP status code
 //!
-//! `http_status_code` provides constants for HTTP status code, which have been defined in [rfc9110](https://www.rfc-editor.org/rfc/rfc9110#name-status-codes).
+//! `rhttp_status_code` provides constants for HTTP status code, which have been defined in [rfc9110](https://www.rfc-editor.org/rfc/rfc9110#name-status-codes).
 //!
 //! ## Usage
 //!
 //! First, add this in your Cargo.toml:
 //! ```toml
 //! [dependencies]
-//! http_status_code = { version = "1" }
+//! rhttp_status_code = { version = "1" }
 //! ```
 //!
 //! Then, you can use it in code, like:
